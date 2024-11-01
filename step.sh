d9b44cc15276f1c94f2500046c98fadded8e6818
Olivér Falvai <ofalvai@gmail.com>
2024-11-01 13:04:47 +0100

    Measure install time

--- step.sh
+++ step.sh
@@ -2,7 +2,10 @@
 
 set -eo pipefail
 
+STEP_ENTRYPOINT="${BITRISE_STEP_SOURCE_DIR}"/step/main.ts
+STEP_DENO_CONFIG="${BITRISE_STEP_SOURCE_DIR}"/deno.json
 DENO_VERSION="2.0.4"
+
 DENO_INSTALL_DIR=${TMPDIR}/deno-${DENO_VERSION}
 OS_TYPE=$(uname -s)
 ARCH_TYPE=$(uname -m)
@@ -34,21 +37,29 @@ if [[ ! -d "${DENO_INSTALL_DIR}" ]]; then
     DENO_URL="https://dl.deno.land/release/v${DENO_VERSION}/deno-${TARGET_TUPLE}.zip"
     echo "Downloading $DENO_URL..."
 
+    start_time="$(date -u +%s.%2N)"
     mkdir -p "${DENO_INSTALL_DIR}"
     curl --retry 5 -fsSL "$DENO_URL" -o "${DENO_INSTALL_DIR}"/deno.zip
     unzip "${DENO_INSTALL_DIR}"/deno.zip -d "${DENO_INSTALL_DIR}"
     rm "${DENO_INSTALL_DIR}"/deno.zip
-
     chmod +x "${DENO_INSTALL_DIR}"/deno
-    echo "☑️ Deno is ready at ${DENO_INSTALL_DIR}"
+    end_time="$(date -u +%s.%2N)"
+
+    echo "☑️ Deno installed in $(bc <<<"$end_time-$start_time") seconds"
 else
     echo "☑️ Using existing Deno binary at ${DENO_INSTALL_DIR}"
 fi
 
+
+
+start_time="$(date -u +%s.%2N)"
+"${DENO_INSTALL_DIR}"/deno install --config "$STEP_DENO_CONFIG"
+end_time="$(date -u +%s.%2N)"
+echo "☑️ Dependencies installed in $(bc <<<"$end_time-$start_time") seconds"
+
 printf "\n---------\n\n"
-# TODO: resolve all dependencies before running to make the log output cleaner
 export DENO_NO_UPDATE_CHECK=1
-sudo --preserve-env \
+sudo --preserve-env \ # sudo is only required for this specific step because it edits /etc/hosts
     "${DENO_INSTALL_DIR}"/deno run \
     --allow-read --allow-write --allow-env --allow-net --allow-run \
-    "${BITRISE_STEP_SOURCE_DIR}"/step/main.ts
+    "$STEP_ENTRYPOINT"
