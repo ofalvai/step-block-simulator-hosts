9d6c2c5ea85e2df135e7bce5d2420bea92165530
Olivér Falvai <ofalvai@gmail.com>
2024-11-01 12:00:58 +0100

    Rename var

--- step.sh
+++ step.sh
@@ -3,7 +3,7 @@
 set -eo pipefail
 
 DENO_VERSION="2.0.4"
-DENO_DIR=${TMPDIR}/deno-${DENO_VERSION}
+DENO_INSTALL_DIR=${TMPDIR}/deno-${DENO_VERSION}
 OS_TYPE=$(uname -s)
 ARCH_TYPE=$(uname -m)
 
@@ -30,23 +30,23 @@ fi
 
 
 
-if [[ ! -d "${DENO_DIR}" ]]; then
+if [[ ! -d "${DENO_INSTALL_DIR}" ]]; then
     DENO_URL="https://dl.deno.land/release/v${DENO_VERSION}/deno-${TARGET_TUPLE}.zip"
     echo "Downloading $DENO_URL..."
 
-    mkdir -p "${DENO_DIR}"
-    curl --retry 5 -fsSL "$DENO_URL" -o "${DENO_DIR}"/deno.zip
-    unzip "${DENO_DIR}"/deno.zip -d "${DENO_DIR}"
-    rm "${DENO_DIR}"/deno.zip
+    mkdir -p "${DENO_INSTALL_DIR}"
+    curl --retry 5 -fsSL "$DENO_URL" -o "${DENO_INSTALL_DIR}"/deno.zip
+    unzip "${DENO_INSTALL_DIR}"/deno.zip -d "${DENO_INSTALL_DIR}"
+    rm "${DENO_INSTALL_DIR}"/deno.zip
 
-    chmod +x "${DENO_DIR}"/deno
-    echo "☑️ Deno is ready at ${DENO_DIR}"
+    chmod +x "${DENO_INSTALL_DIR}"/deno
+    echo "☑️ Deno is ready at ${DENO_INSTALL_DIR}"
 else
-    echo "☑️ Using existing Deno binary at ${DENO_DIR}"
+    echo "☑️ Using existing Deno binary at ${DENO_INSTALL_DIR}"
 fi
 
 printf "\n---------\n\n"
 export DENO_NO_UPDATE_CHECK=1
-"${DENO_DIR}"/deno run \
+"${DENO_INSTALL_DIR}"/deno run \
     --allow-read --allow-write --allow-env --allow-net --allow-run \
     step/main.ts
